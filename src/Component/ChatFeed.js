7c4a428a74de1397381b9fe6104facdf9266cb18
olamiji-8 <olamijisotomi@gmail.com>
2022-10-14 11:37:29 +0100

    update

--- src/Component/ChatFeed.js
+++ src/Component/ChatFeed.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import MessageForm from './MessageForm'
+import MyMessage from './MyMessage'
+import TheirMessage from './TheirMessage'
 
 const ChatFeed = () => {
   return (
