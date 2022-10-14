326742b6da9b2d1d125327caf20a1bd58a6c5d5b
olamiji-8 <olamijisotomi@gmail.com>
2022-10-14 02:41:50 +0100

    update

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
     <div>
       <ChatEngine
       height = "100vh"
-      projectID = ""
-      userName = ""
-      userSecret = ""
+      projectID = "1b98f23c-70e1-4433-812a-02fffe855eaa"
+      userName = "Olapelu"
+      userSecret = "123"
       />
     </div>
     
